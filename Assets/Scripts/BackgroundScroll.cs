a5630e10727121e4e2b4c7a6b7d9c27411354324
unknown <jeffreyheng96@yahoo.com>
2019-02-18 00:35:45 -0800

    added background and score

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    Material material;
+    Vector2 move;
+
+    public int xVelocity, yVelocity;
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        move = new Vector2(xVelocity, yVelocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        material.mainTextureOffset += (move * Time.deltaTime) / 15;
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text scoreText;
+    public Text highscoreText;
+    public float scoreCount;
+    public float highscoreCount;
+    public float points;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreCount += points * Time.deltaTime;
+        if (scoreCount > highscoreCount)
+        {
+            highscoreCount = scoreCount;
+        }
+        scoreText.text = "Score: " + Mathf.Round(scoreCount);
+        highscoreText.text = "High Score: " + Mathf.Round(highscoreCount);
+
+    }
+}
