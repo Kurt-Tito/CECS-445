49dc866a03fa7baab85faaeffa59fccfd3834b41
Kurt Tito <kurtcedricktito@gmail.com>
2019-02-25 20:20:18 -0800

    Added varied platforsm with varying obstacle placements. Each obstacle has an onTrigger Capsule Collider

--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class SpawnerScript : MonoBehaviour {
 
-    public GameObject floor;
-    public float next_spawn_time;
+    public GameObject floor_var1, floor_var2, floor_var3;
+    private float next_spawn_time;
 
 	// Use this for initialization
 	void Start () {
-        next_spawn_time = 4.75f;
+        next_spawn_time = 1.35f;
 	}
 	
 	// Update is called once per frame
@@ -18,12 +18,30 @@ public class SpawnerScript : MonoBehaviour {
         if (Time.time > next_spawn_time)
         {
             spawnFloor();
-            next_spawn_time += 4.75f;
+            next_spawn_time += 4.5f;
         }
 	}
 
     public void spawnFloor()
     {
-        Instantiate(floor, transform.position, Quaternion.identity);
+        int roll = Random.Range(1, 3);
+
+        switch (roll)
+        {
+            case 1:
+                Instantiate(floor_var1, transform.position, Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(floor_var2, transform.position, Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(floor_var3, transform.position, Quaternion.identity);
+                break;
+            default:
+                Debug.Log("Could not load floor object");
+                break;
+        }
+
+        //Instantiate(floor_var1, transform.position, Quaternion.identity);
     }
 }
