c44eeaa8ebe466883d49f9d188d5029927027c3f
Kurt Tito <kurtcedricktito@gmail.com>
2019-02-26 00:20:09 -0800

    Implemented hit collision and player lives

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,6 +7,7 @@ public class PlayerScript : MonoBehaviour {
     private Rigidbody2D player;
     private Vector3 direction = new Vector3(0.0f, 1.0f, 0.0f);
     private float force = 35.0f;
+    public static int playerLives = 3;
     //private int jumpCount = 0;
 
     //private bool isGrounded = false;
@@ -19,11 +20,22 @@ public class PlayerScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-		if (Input.GetKeyDown(KeyCode.Space) && (player.velocity.y > -0.25 && player.velocity.y < 0.25)) //checks to maker sure player is grounded before pressing jump
+		if (Input.GetKeyDown(KeyCode.Space) && (player.velocity.y > -0.2 && player.velocity.y < 0.2)) //checks to maker sure player is grounded before pressing jump
         {
             Debug.Log("JUMPED");
             player.AddForce(direction * force, ForceMode2D.Impulse);
             //player.transform.position += direction;
         }
 	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "obstacle")
+        {
+            Debug.Log("player hit");
+
+            if (playerLives > 0)
+                playerLives--;
+        }
+    }
 }
--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -24,7 +24,7 @@ public class SpawnerScript : MonoBehaviour {
 
     public void spawnFloor()
     {
-        int roll = Random.Range(1, 3);
+        int roll = Random.Range(1, 4);
 
         switch (roll)
         {
